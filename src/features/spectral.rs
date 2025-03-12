b63215d6bd1d8a45e13d9918dd60fa6091c766df
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2025-03-12 12:36:08 +0330

    Add constant-q chromagram feature extraction

--- src/features/spectral.rs
+++ src/features/spectral.rs
@@ -36,3 +36,34 @@ pub fn chroma_stft(
     }
     chroma
 }
+
+pub fn chroma_cqt(
+    y: Option<&[f32]>,
+    sr: Option<u32>,
+    C: Option<&Array2<f32>>,
+    hop_length: Option<usize>,
+    fmin: Option<f32>,
+    bins_per_octave: Option<usize>,
+) -> Array2<f32> {
+    let sr = sr.unwrap_or(44100);
+    let hop = hop_length.unwrap_or(512);
+    let fmin = fmin.unwrap_or(32.70);
+    let bpo = bins_per_octave.unwrap_or(12);
+    let n_bins = bpo * 3;
+    let C = match (y, C) {
+        (Some(y), None) => cqt(y, sr, Some(hop), Some(fmin), Some(n_bins), Some(bpo)).unwrap(),
+        (None, Some(C)) => C.to_owned(),
+        _ => panic!("Must provide either y or C"),
+    };
+    let mut chroma = Array2::zeros((12, C.shape()[1]));
+    for frame in 0..C.shape()[1] {
+        for bin in 0..C.shape()[0] {
+            let freq = fmin * 2.0f32.powf(bin as f32 / bpo as f32);
+            let midi = hz_to_midi(&[freq])[0];
+            let pitch_class = (midi.round() as usize % 12) as usize;
+            chroma[[pitch_class, frame]] += C[[bin, frame]].norm();
+        }
+    }
+    chroma
+}
+
