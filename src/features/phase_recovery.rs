e87ef7d351a0c1bef12c5b09f2efd19668e00581
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2025-03-12 03:52:09 +0330

    Update griffinlim Vec<f32> instead of Array1

--- src/features/phase_recovery.rs
+++ src/features/phase_recovery.rs
@@ -6,22 +6,22 @@ pub fn griffinlim(S: &Array2<f32>, n_iter: Option<usize>, hop_length: Option<usi
     let hop = hop_length.unwrap_or(n_fft / 4).max(1);
     let n_iter = n_iter.unwrap_or(32);
     let signal_len = hop * (S.shape()[1] - 1) + n_fft;
-    let mut y = Array1::from_vec(crate::signal_generation::tone(440.0, None, Some(signal_len), None, None));
+    let mut y = crate::signal_generation::tone(440.0, None, Some(signal_len), None, None);
     for _ in 0..n_iter {
-        let stft_y = crate::signal_processing::stft(y.as_slice().unwrap(), Some(n_fft), Some(hop), None).unwrap();
+        let stft_y = crate::signal_processing::stft(&y, Some(n_fft), Some(hop), None).unwrap();
         let (mut mag, mut phase) = crate::signal_processing::magphase(&stft_y, None);
         for ((i, j), m) in mag.indexed_iter_mut() {
             *m = S[[i, j]].sqrt();
             let p = &mut phase[[i, j]];
-            if m.abs() > 1e-10 { 
+            if m.abs() > 1e-10 {
                 *p = *p / p.norm();
             }
         }
         let mag_complex = mag.mapv(|x| Complex::new(x, 0.0));
         let new_stft = mag_complex * phase;
-        y = Array1::from_vec(crate::signal_processing::istft(&new_stft, Some(hop), None, Some(signal_len)));
+        y = crate::signal_processing::istft(&new_stft, Some(hop), None, Some(signal_len));
     }
-    y.to_vec()
+    y
 }
 
 pub fn griffinlim_cqt(_C: &Array2<f32>, _n_iter: Option<usize>, _sr: Option<u32>) -> Vec<f32> { unimplemented!() }
